d402dabcad734ee31f3585629ace701634f43a80
Luis Aguilera <aguilera.glz.luis@gmail.com>
2021-06-03 19:10:26 -0500

    Comment on main_goroutines array-manipulation problem

--- array-manipulation/main_goroutines.go
+++ array-manipulation/main_goroutines.go
@@ -21,7 +21,7 @@ import (
 // TODO:
 // 10/16 test cases failed, pasing: [0, 1, 2, 3, 14, 15]
 // 4. OUTPUT: 7540746475, ( variant)
-// 7. TimeLimit exceded
+// 7. TimeLimit exceded, however, output is correct: 2497169732
 func arrayManipulation(n int32, queries [][]int32) int64 {
 	// Write your code here
 	arr := make([]int64, n)
